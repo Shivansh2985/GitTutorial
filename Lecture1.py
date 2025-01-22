412c5b8312edca1001226f95c145fbec2de93158
Shivansh2985 <shivanshsharma23000@gmail.com>
2025-01-22 20:51:16 +0530

    Python Lecture

---
+++ Lecture1.py
@@ -0,0 +1,43 @@
+#string
+
+a = "name"
+print(a) #prints the value assigns
+print(type(a)) #prints the type of the variable
+
+#integer
+
+a = 5
+print(a) #prints the value assigns
+print(type(a)) #prints the type of the variable
+
+#float
+
+a = 5.4
+print(a) #prints the value assigns
+print(type(a)) #prints the type of the variable
+
+#Boolean
+
+a = True
+print(a) #prints the value assigns
+print(type(a)) #prints the type of the variable
+
+#Input
+
+a = int(input("Enter your name: "))#takes the input
+print(a)
+print(type(a))
+
+
+#Swapping
+
+a = 5
+b = 8
+
+print(a)
+print(b)
+a = a^b
+b = a^b
+a = a^b
+print(a)
+print(b)
